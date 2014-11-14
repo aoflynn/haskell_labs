b1159578fa701c2926b42881a23442e0f9519fbd
aoflynn <adam.oflynn7@mail.dcu.ie>
2014-11-14 23:56:53 +0000

    Update shortest.hs

--- lab_2/shortest.hs
+++ lab_2/shortest.hs
@@ -4,9 +4,5 @@ shortest :: [[a]] -> [a]
 shortest [] = []
 shortest [x] = x
 shortest (x:xs) = if length x < length (shortest xs)
-								  then x
-								  else shortest xs
-
-
-
-
+	  	  then x
+	          else shortest xs
