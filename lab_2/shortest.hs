c6de68f9d4c1f2df52dc6706835c1727e06edd37
Adam <aoflynn9909@gmail.com>
2014-11-14 23:53:18 +0000

    Completed shortest

--- lab_2/shortest.hs
+++ lab_2/shortest.hs
@@ -1,3 +1,12 @@
+-- find shortest list in a list --
+
 shortest :: [[a]] -> [a]
-shortest [[]] = []
+shortest [] = []
+shortest [x] = x
+shortest (x:xs) = if length x < length (shortest xs)
+								  then x
+								  else shortest xs
+
+
+
 
